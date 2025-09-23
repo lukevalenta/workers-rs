5f3f8feec1b64d81c37d68a2063ea223a6f053d4
Luke Valenta <lvalenta@cloudflare.com>
2025-09-23 14:43:49 -0400

    Clean up formatting for JS worker

--- do-alarm-memory-leak/js/src/index.js
+++ do-alarm-memory-leak/js/src/index.js
@@ -7,7 +7,7 @@ export class MyDurableObject extends DurableObject {
 	}
 
 	async fetch() {
-    await this.storage.setAlarm(Date.now() + 1);
+    	await this.storage.setAlarm(Date.now() + 1);
 		return new Response();
 	}
 
@@ -19,10 +19,9 @@ export class MyDurableObject extends DurableObject {
 
 export default {
 	async fetch(request, env, ctx) {
-    console.log('fetch');
 		const id = env.MY_DURABLE_OBJECT.idFromName("foo");
 		const stub = env.MY_DURABLE_OBJECT.get(id);
 		await stub.fetch("http://example.com");
-        return new Response('Initialized Durable Object ' + id)
-	},
+        return new Response('Initialized Durable Object ' + id);
+	}
 };
